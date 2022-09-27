6708b00b0d02166fb5fb988dea069d4395cf8bde
DRAINER <113362895+JakeFlakne10@users.noreply.github.com>
2022-09-27 09:07:04 -0500

    q3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from time import *
 
-def clear()
+def clear():
     os.system('cls')
 
 def Question1():
@@ -50,3 +50,28 @@ def Question2():
         print("INVALID INPUT")
         clear()
         start()
+
+def Question3():
+    #Story
+    clear()
+    print("Gaming is fun again! life still sucks tho.")
+
+    #Question 3
+    a3 = input("Do you try to learn bcs intro on your brothers guitar?\n\Y\nN\n")
+
+    #input Y
+    if a3 == "Y":
+        Question4()
+
+    #input N
+    elif a3 == "N":
+        print("You lose interest in your hobbies, become mentally ill agian, and fail at life.")
+
+    #invalid input
+    else:
+        print("INVALID INPUT")
+        clear()
+        start()
+
+def Question4():
+    print("The enevitable happens, there are no more endings, you create your own")
